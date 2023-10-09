721c1c9c195a2f70373f68365a7508102813c31b
aaronlyy <aaronlevican@gmail.com>
2023-10-09 17:09:11 +0200

    Added methods to be importable

--- pyhwid/__init__.py
+++ pyhwid/__init__.py
@@ -1,7 +1,12 @@
 from .core import PyHWID
 from .wrapper import generate_hwid
+from .mac import get_mac_address
+from .cpu import get_cpu_name, get_cpu_cores
 
 __all__ = [
   'PyHWID',
-  'generate_hwid'
+  'generate_hwid',
+  'get_mac_address',
+  'get_cpu_name',
+  'get_cpu_cores'
 ]
