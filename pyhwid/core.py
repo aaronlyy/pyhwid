65fc8078a3e33549243935e6ea2890efd4b2cd0e
aaronlyy <aaronlevican@gmail.com>
2023-10-09 16:59:40 +0200

    Removed methods

--- pyhwid/core.py
+++ pyhwid/core.py
@@ -1,30 +1,19 @@
-from uuid import getnode
+import platform
+
+from .mac import get_mac_address
+
+from .cpu import get_cpu_cores
+from .cpu import get_cpu_name
 
 
 class PyHWID:
   def __init__(self):
+    self.platform = platform.system()
     pass
 
   def generate(self):
     pass
 
-  def get_mac_address(self) -> str:
-    """Get the MAC address as a 48bit integer
-
-    Note: Using this methods MAC address as part of an HWID is unsafe
-    because the uuid package fakes the MAC address if it is unable to get the actual address
-
-    Returns:
-        str: MAC address as 48bit integer
-    """
-    mac_address = getnode() # use uuid to get the mac address as 48bit integer
-    return mac_address
-
-  def get_cpu(self) -> str:
-    pass
-
-  def get_gpu(self) -> str:
-    pass
-
 x = PyHWID()
 print(x.get_mac_address())
+print(x.get_cpu())
